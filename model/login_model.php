a33ded6dca92211f313605b04cec4e97205d382c
sayasukacereal <williaml1601@gmail.com>
2020-03-13 11:39:42 +0700

    idk what i just do
    pls elp

---
+++ model/login_model.php
@@ -0,0 +1,28 @@
+<?php
+class loginCheck
+{
+    var $username;
+    var $password;
+    function __construct($susername, $spassword)
+    {
+        $this->username = $susername;
+
+        $this->password = $spassword;
+    }
+    public function getusername()
+    {
+        return $this->username;
+    }
+    public function getpassword()
+    {
+        return $this->password;
+    }
+    public function setusername($str)
+    {
+        $this->username    = $str;
+    }
+    public function setpassword($str)
+    {
+        $this->password = $str;
+    }
+}
