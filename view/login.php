7dca56606cbb51649679229d4b5b64bdd48e8bb5
sayasukacereal <williaml1601@gmail.com>
2020-03-13 19:51:18 +0700

    update

--- view/login.php
+++ view/login.php
@@ -15,12 +15,14 @@
 
     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
         integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Pacifico|Roboto&display=swap" rel="stylesheet">
 
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
 <body>
@@ -38,12 +40,14 @@
                             <div class="form-label-group">
                                 <input type="text" id="inputUsername" class="form-control" placeholder="Username"
                                     required autofocus name="username">
+                                <input type="text" id="inputUsername" class="form-control" placeholder="Username" required autofocus name="username">
                                 <label for="username">Username</label>
                             </div>
 
                             <div class="form-label-group">
                                 <input type="password" id="inputPassword" class="form-control" placeholder="Password"
                                     required name="password">
+                                <input type="password" id="inputPassword" class="form-control" placeholder="Password" required name="password">
                                 <label for="inputPassword">Password</label>
                             </div>
 
@@ -53,14 +57,18 @@
                             </div>
                             <?php if (isset($invalid) && $invalid) { ?>
                             <p class='text-danger'>Invalid credentials.</p>
+                                <p class='text-danger'>Invalid credentials.</p>
                             <?php } ?>
                             <input type="hidden" name="do" value="login_check.php">
                             <input class="btn btn-lg btn-primary btn-block text-uppercase" type="submit" name="submit"
                                 value="Sign In">
+                            <input class="btn btn-lg btn-primary btn-block text-uppercase" type="submit" name="submit" value="Sign In">
                         </form>
                         <form class="form-signin" method="POST">
                             <button name='loc' value="register.php"
                                 class='btn btn-lg btn-block btn-warning text-uppercase'>Register</button>
+                        <form class="form-signin" method="POST" style="margin-top: 10px">
+                            <button name='loc' value="register.php" class='btn btn-lg btn-block btn-warning text-uppercase'>Register</button>
                         </form>
                     </div>
                 </div>
@@ -75,12 +83,16 @@
         integrity="sha256-MAgcygDRahs+F/Nk5Vz387whB4kSK9NXlDN3w58LLq0=" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/js/all.min.js" integrity="sha256-MAgcygDRahs+F/Nk5Vz387whB4kSK9NXlDN3w58LLq0=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous">
     </script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
         integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
     </script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
         integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
     </script>
 </body>
 
