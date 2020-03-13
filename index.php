6da67377bfe47ed36898f27314ac826379e74fb0
sayasukacereal <williaml1601@gmail.com>
2020-03-13 15:26:14 +0700

    idk what happen just rollback if something bad happens
    test

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 if (isset($_POST['loc'])) $loc = $_POST['loc'];
-else $loc = 'home.php';
-include 'include/db_connect.php';
-include 'model/User.php';
+else $loc = 'login.php';
+// include 'include/db_connect.php';
+// include 'model/User.php';
+include 'view/' . $loc;
---
+++ model/user.php
@@ -0,0 +1,107 @@
+<?php
+class user
+{
+    var $username;
+    var $firstName;
+    var $lastName;
+    var $password;
+    var $bdate;
+    var $gender;
+    var $profilePicturePath;
+    var $coverPath;
+    var $contact;
+    var $userdesc;
+    function __construct($susername, $sfirstName, $slastName, $spassword, $sbdate, $sgender, $sprofilePicturePath, $scoverPath, $scontact, $suserdesc)
+    {
+        $this->username = $susername;
+        $this->firstName = $sfirstName;
+        $this->lastName = $slastName;
+        $this->password = $spassword;
+        $this->bdate = $sbdate;
+        $this->gender = $sgender;
+        $this->profilePicturePath = $sprofilePicturePath;
+        $this->coverPath = $scoverPath;
+        $this->contact = $scontact;
+        $this->userdesc = $suserdesc;
+    }
+    public function getusername()
+    {
+        return $this->username;
+    }
+    public function getfirstName()
+    {
+        return $this->firstName;
+    }
+    public function getlastName()
+    {
+        return $this->lastName;
+    }
+    public function getpassword()
+    {
+        return $this->password;
+    }
+    public function getbdate()
+    {
+        return $this->bdate;
+    }
+    public function getgender()
+    {
+        return $this->gender;
+    }
+    public function getprofilePicturePath()
+    {
+        return $this->profilePicturePath;
+    }
+    public function getcoverPath()
+    {
+        return $this->coverPath;
+    }
+    public function getcontact()
+    {
+        return $this->contact;
+    }
+    public function getuserdesc()
+    {
+        return $this->userdesc;
+    }
+    public function setusername($str)
+    {
+        $this->username    = $str;
+    }
+    public function setfirstName($str)
+    {
+        $this->firstName   = $str;
+    }
+    public function setlastName($str)
+    {
+        $this->lastName   = $str;
+    }
+    public function setpassword($str)
+    {
+        $this->password = $str;
+    }
+    public function setbdate($str)
+    {
+        $this->bdate = $str;
+    }
+    public function setgender($str)
+    {
+        $this->gender = $str;
+    }
+    public function setprofilePicturePath($str)
+    {
+        $this->profilePicturePath = $str;
+    }
+    public function setcoverPath($str)
+    {
+        $this->coverPath = $str;
+    }
+    public function setcontact($str)
+    {
+        $this->contact = $str;
+    }
+    public function setuserdesc($str)
+    {
+        $this->userdesc = $str;
+    }
+}
