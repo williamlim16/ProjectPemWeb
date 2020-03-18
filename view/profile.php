ca37b48468c6a2291b4737137d9c50ec777cf5e5
MinorvaFalk <minovfalk@gmail.com>
2020-03-18 21:57:03 +0700

    Push again

--- view/profile.php
+++ view/profile.php
@@ -27,6 +27,9 @@ $users = new User(
     $result2['userdesc']
 );
 
+$query3 = "SELECT * FROM comment WHERE username = '" . $_SESSION['user']->getusername() . "'";
+$result3 = $conn->query($query2)
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -225,7 +228,7 @@ $users = new User(
                     foreach ($posts as $row) {
                         
                         echo
-                            '<div class="w3-container w3-card w3-white w3-round w3-margin"><br />
+                        '<div class="w3-container w3-card w3-white w3-round w3-margin"><br />
                         <img src="' . $row->getPicture()  . '" alt="avatar here" class="w3-left w3-margin-right postPicSize" style="width:60px" />
                         <span class="w3-right w3-opacity">' . $row->getTimestamp() . '</span>
                         <h4>' . $row->getUsername() . '</h4><br />
@@ -233,7 +236,7 @@ $users = new User(
                         <p>' . $row->getContent() . '</p>
                         <button type="button" class="w3-button w3-theme-d1 w3-margin-bottom"><i class="fa fa-thumbs-up"></i> Like</button>
                         <button type="button" data-toggle="collapse" data-target="#collapseExample' . $row->getId() . '" class="w3-button w3-theme-d2 w3-margin-bottom"><i class="fa fa-comment"></i> Comment</button>
-                        <div class="collapse" id="collapseExample' . $row->getId().'">
+                        <div class="collapse" id="collapseExample'. $row->getId().'">
                             <div class="card card-body">
                                 Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident.
                             </div>
